f2459be270b7700827ab8b2a81f1508c12a4b978
Jake Day Williams <jake@wildacademy.com>
2016-11-29 01:06:41 -0700

    Changes to styling and truncation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :find_item, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     if user_signed_in?
       @items = Item.where(user_id: current_user.id).order("created_at DESC")
@@ -9,11 +9,11 @@ class ItemsController < ApplicationController
 
   def show
   end
- 
+
   def new
     @item = current_user.items.build
   end
-  
+
   def create
     @item = current_user.items.build(item_params)
     if @item.save
@@ -22,10 +22,10 @@ class ItemsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
   end
-  
+
   def update
     if @item.update(item_params)
       redirect_to item_path(@item)
@@ -33,27 +33,30 @@ class ItemsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @item.destroy
     redirect_to root_path
   end
-  
+
   def complete
-    @item = Item.find(param[:id])
-    @item.update_attribute(:completed_at, Time.now)
+    @item = Item.find(params[:id])
+    if @item.completed_at?
+      @item.update_attribute(:completed_at, nil)
+    else
+      @item.update_attribute(:completed_at, Time.now)
+    end
     redirect_to root_path
   end
-  
-  
+
   private
-    
+
     def item_params
       params.require(:item).permit(:title, :description)
     end
-    
+
     def find_item
       @item = Item.find(params[:id])
-    end 
-    
+    end
+
 end
